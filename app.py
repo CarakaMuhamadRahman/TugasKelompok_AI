02ad83c681b4b9887a91381cb6f8b73ef75050aa
RidhwanAF <72844854+RidhwanAF@users.noreply.github.com>
2023-01-15 15:59:14 +0700

    adding cross origin for access using api on react

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
 import pickle
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 model = pickle.load(open('model.pkl','rb'))
 
 @app.route("/")
+@cross_origin
 def home():
     return  'This API for Playing Golf Reccomendation Application'
 
 
+@cross_origin 
 @app.route("/predict", methods=["POST"])
 def predict():
     json_ = request.json
@@ -19,4 +25,4 @@ def predict():
     return jsonify({'Prediction': list(prediction)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
